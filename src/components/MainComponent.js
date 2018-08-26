309993e79e754506d10cc46017270ca8c8e4e574
abeerabdelsamee <abeer.abdelsamee@gmail.com>
2018-08-26 13:37:10 +0200

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,9 +5,13 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 // import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -15,6 +19,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS,
         selectedDish: null
     };
   }
@@ -30,15 +37,11 @@ class Main extends Component {
       )
     }
     return (
-      <div>
-      <Header />
-      <Switch>
-      <Route path='/home' component={HomePage}/>
-      <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-      <Redirect to="/menu" />
-      </Switch>
-      <Footer />
-        </div>
+      <Home 
+      dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+  />
     );
   }
 }
